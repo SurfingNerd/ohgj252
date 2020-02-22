68149424651f65b18efa5ab2f097f513bfe1e89c
Thomas Haller <Thomas.Haller@lab10.coop>
2020-02-22 22:13:37 +0100

    basic infrastructure

---
+++ Assets/Scripts/BatteryScript.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryScript : MonoBehaviour
+{
+    public GameObject infoGameObject;
+    // Start is called before the first frame update
+
+    private GameObject playerInRange = null;
+    private bool isMounted = false;
+    private Transform originaParent = null;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange != null && Input.GetKey(KeyCode.E))
+        {
+            if (isMounted)
+            {
+                Debug.Log("Unmounted");
+                this.transform.SetParent(originaParent);
+                isMounted = false;
+                // todo: Unmount.
+            }
+            else
+            {
+                // mount.
+                isMounted = true;
+                originaParent = this.transform.parent;
+                this.transform.SetParent(playerInRange.transform);
+                Debug.Log("Mounted");
+            }
+        }
+    }
+    
+    
+    //When the Primitive collides with the walls, it will reverse direction
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entered: " + other.gameObject.name + " " + other.tag);
+        if (other.CompareTag("Player"))
+        {
+            infoGameObject.SetActive(true);
+            playerInRange = other.gameObject;
+        }
+        
+        //this.gameObject.transform.
+    }
+
+    //When the Primitive exits the collision, it will change Color
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Exited: " + other.gameObject.name + " " + other.tag);
+        if (other.CompareTag("Player"))
+        {
+            infoGameObject.SetActive(false);
+            if (!isMounted)
+            {
+                playerInRange = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Foen.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foen : MonoBehaviour
+{
+
+    public float radius = 3;
+
+    private Rigidbody2D playerInRange;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //When the Primitive collides with the walls, it will reverse direction
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Foen: Entered: " + other.gameObject.name + " " + other.tag);
+            //infoGameObject.SetActive(true);
+            playerInRange = other.gameObject.GetComponent<Rigidbody2D>();
+        }
+        
+        //this.gameObject.transform.
+    }
+
+    //When the Primitive exits the collision, it will change Color
+    private void OnTriggerExit2D(Collider2D other)
+    {
+       
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(" Foen: Exited: " + other.gameObject.name + " " + other.tag);
+            playerInRange = null;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (playerInRange)
+        {
+
+            float distance = Vector3.Distance(this.transform.position, playerInRange.transform.position);
+            Vector2 force = Vector2.up * Time.fixedDeltaTime * (10000 / distance);
+            Debug.Log("Adding FOrce: " + force);
+            playerInRange.AddForce(force , ForceMode2D.Force);
+        }
+    }
+}
